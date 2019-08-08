617987692c9782ca38a7201041b61bc63ab42d17
vanche93 <vanche93@yandex.ru>
2019-08-08 17:04:41 +0300

    Перенес ссылку на расположения скрипта в config.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
+
 from __future__ import print_function
 from rutimeparser import parse
 from rutimeparser import get_clear_text, get_last_clear_text
 from datetime import datetime, date, time
 from parser import import_dt, import_text
 from bot import bot
+from config import sdir
 import subprocess
 import re
 import sys
 import telebot
 #import datetime
 import logging
-from os import getcwd
-#Рабочая папка срипта
-dir = getcwd()
 #Логирование
 logging.basicConfig(filename="bremindbot.log", level=logging.INFO)
 logtime = datetime.now()
@@ -43,7 +42,7 @@ def send_message(id, text):
     bot.send_message(id, text)
 #Функция создают задачу в AT
 def add_task(id, text, date_time):
-    cmd = 'echo "%s/send_message.py %s  \'%s\''' " | at %s' % (dir, id, text, date_time)
+    cmd = 'echo "%s/send_message.py %s  \'%s\''' " | at %s' % (sdir, id, text, date_time)
     logging.info(logtime)
     logging.info (cmd)
     subprocess.Popen(cmd, shell=True)
