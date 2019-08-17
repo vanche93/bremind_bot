5beaf0f5443b2511162b86c2eb2ec04c4388b281
vanche93 <vanche93@yandex.ru>
2019-08-17 22:45:06 +0300

    Все новые задачи добавляются в бд

--- db.py
+++ db.py
@@ -1,44 +1,55 @@
 import sqlite3
 
-conn = sqlite3.connect("db.db")
-cursor = conn.cursor()
-
-#Функция добавляет данные в таблицу 'tz'
+# Функция добавляет данные в таблицу 'tz'
 def add_to_db_tz(id, username, timezone):
-    #Если таблицы не существует создать ее
+    conn = sqlite3.connect("db.db")
+    cursor = conn.cursor()
+    # Если таблицы не существует создать ее
     cursor.execute("""CREATE TABLE IF NOT EXISTS tz
                           (id text, username text, timezone text)
                        """)
     ins = """INSERT INTO tz VALUES ('%s', '%s', '%s')""" % (id, username, timezone)
     cursor.execute(ins)
     conn.commit()
-#Чтение данных из таблицы 'tz'
+
+
+# Чтение данных из таблицы 'tz'
 def read_data_in_tz(chatid):
+    conn = sqlite3.connect("db.db")
+    cursor = conn.cursor()
     cursor.execute('SELECT * FROM tz WHERE chatid LIKE ?', [chatid])
-    result =  cursor.fetchall()
+    result = cursor.fetchall()
     print(result)
 
-#Функция добавляет данные в таблицу 'tasklist'
-def add_to_db_tasklist(chatid, number, time, text):
-    #Если таблицы не существует создать ее
+# Функция создает таблицу 'tasklist'
+def greate_db_tasklist(chatid):
+    conn = sqlite3.connect("db.db")
+    cursor = conn.cursor()
+    chatid = 'tasklist:' + str(chatid)
+    # Если таблицы не существует создать ее
     cursor.execute("""CREATE TABLE IF NOT EXISTS '%s'
                           (number text, time text, text text)
                        """ % (chatid))
+
+# Функция добавляет данные в таблицу 'tasklist'
+def add_to_db_tasklist(chatid, number, time, text):
+    conn = sqlite3.connect("db.db")
+    cursor = conn.cursor()
     chatid = 'tasklist:' + str(chatid)
+    # Если таблицы не существует создать ее
+    cursor.execute("""CREATE TABLE IF NOT EXISTS '%s'
+                          (number text, time text, text text)
+                       """ % (chatid))
     ins = """INSERT INTO '%s'  VALUES ('%s', '%s', '%s')""" % (chatid, number, time, text)
     cursor.execute(ins)
     conn.commit()
-#Чтение данных из таблицы 'tasklist'
+
+# Чтение данных из таблицы 'tasklist'
 def read_data_in_task(chatid):
+    conn = sqlite3.connect("db.db")
+    cursor = conn.cursor()
     chatid = 'tasklist:' + str(chatid)
-    cursor.execute("""SELECT * FROM '%s'""" % (chatid))
-    result =  cursor.fetchall()
+    c = cursor.execute("""SELECT * FROM '%s'""" % (chatid))
+    result = '*Номер задачи | Время | Задача* \n' + '\n'.join(['| '.join(map(str, x)) for x in c])
     print(result)
-
-
-#add_to_db('ид3','user3','Amsterdam3')
-#read_data_in_tz('ид')
-#add_to_db_tasklist('777777', '2', '16:30', 'test')
-#read_data_in_task('777777')
-
-#print(cursor.execute("""SELECT name FROM sqlite_master WHERE type='table' AND name='tasklist:777777'"""))
+    return result
--- main.py
+++ main.py
@@ -1,41 +1,55 @@
-
 from __future__ import print_function
 from parser import import_dt, import_text
 from bot import bot
 from config import sdir
+from db import add_to_db_tasklist, read_data_in_task, greate_db_tasklist
 import subprocess
 import re
+
+
 #Функция отвечает на стартовое сообщение
-@bot.message_handler(commands=['start', 'help',])
+@bot.message_handler(commands=['start', 'help'])
 def start_message(message):
+    greate_db_tasklist(message.chat.id)
     bot.send_message(message.chat.id,
                      'Привет, я бот котрый напомнит тебе что то сделать. \n Просто напиши мне что и когда тебе напомнить. \n Например "Выпить таблетки завтра днем" или "Забрать заказ 13 октября" \n Пока бот не умеет удалять созданые напоминания и часовой пояс MSK +3, но скоро я это все поправлю.')
-#Функция обрабатывает текстовые сообщения
+
+# Функция отвечает на стартовое сообщение
+@bot.message_handler(commands=['tasklist'])
+def start_message(message):
+    text = read_data_in_task(message.chat.id)
+    print(text)
+    bot.send_message(message.chat.id, text, parse_mode= 'Markdown')
+# Функция обрабатывает текстовые сообщения
 @bot.message_handler(content_types=['text'])
 def in_text(message):
-    #bot.send_message(message.chat.id, 'Я жив!')
     dt = import_dt(message.text)
     text = import_text(message.text)
-    if dt == 'null' :
+    if dt == 'null':
         bot.send_message(message.chat.id, 'Нужно указать время!')
     elif dt == 'old':
         bot.send_message(message.chat.id, 'Это уже в прошлом!')
     elif text == 'null':
         bot.send_message(message.chat.id, 'Нужно указать о чем вам напомнить!')
     else:
-        add_task(message.chat.id,text, dt)
+        add_task(message.chat.id, text, dt)
         answer = 'Я напомню тебе ' + text + ' ' + dt
         bot.send_message(message.chat.id, answer)
-#Функция отправляет сообщения в чат пользователю
+
+# Функция отправляет сообщения в чат пользователю
 def send_message(id, text):
     bot.send_message(id, text)
-#Функция создают задачу в AT и добавляет ее в бд
+
+# Функция создают задачу в AT и добавляет ее в бд
 def add_task(id, text, date_time):
     cmd = 'echo "%s/send_message.py %s  \'%s\''' " | at %s' % (sdir, id, text, date_time)
     print(cmd)
     out = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    stdout = out.communicate()
+    stdout = str(out.communicate())
     print(stdout)
+    number = re.search('job(.+?) at', stdout).group(1)
+    add_to_db_tasklist(id, number, date_time, text)
 
-#Ожидать входящие сообщения
+
+# Ожидать входящие сообщения
 bot.polling()
