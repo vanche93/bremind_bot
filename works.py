a3bba921ab5d7d7f074f47b5ebc81e403ef96b5c
vanche93 <vanche93@yandex.ru>
2019-09-11 11:05:16 +0300

    init

--- works.py
+++
@@ -1,9 +0,0 @@
-import telebot
-from config import token
-bot = telebot.TeleBot(token)
-
-@bot.message_handler(content_types=['text'])  # Функция обрабатывает текстовые сообщения
-def in_text(message):
-    bot.send_message(message.chat.id, 'Ведуться технические работы')
-
-bot.polling()
